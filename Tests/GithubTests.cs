29ec82a66bbf1d7bbed90b69ff75bb5c7098c166
kokhany <yevhen.kokhan@gmail.com>
2018-01-16 02:53:51 +0200

    code

---
+++ Tests/GithubTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Tests
+{
+    [TestFixture]
+    public class GithubTests
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Contains_sign_up_page()
+        {
+            _driver.Url = "https://github.com";
+
+            IWebElement element = _driver.FindElement(By.CssSelector(".home-hero-signup"));
+
+            Assert.IsNotNull(element);
+        }
+
+        [TearDown]
+        public void AfterEach()
+        {
+            _driver.Quit();
+        }
+    }
+}
